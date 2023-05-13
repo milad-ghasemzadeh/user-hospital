d9964cb6a4336b348aa35775927c71ce18ae3f18
Milad <milad.ghasemzadeh74@gmail.com>
2023-05-14 00:11:58 +0330

    Adding new Files

---
+++ src/test/java/com/hospital/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
